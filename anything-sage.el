8335657d3f0611bd5fc8858160bbe87dfdbaf0f2
Sho Takemori <stakemorii@gmail.com>
2014-10-05 20:13:15 +0900

    Added commentary.

--- anything-sage.el
+++ anything-sage.el
@@ -22,8 +22,9 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-;;; Setting
-;; Bind commands to some keys: e.g.
+;;; Commentary:
+;; anything-sage provides 3 commands.
+;; Bind them to some keys: e.g.
 ;; (defun anything-sage-set-up ()
 ;;   (local-set-key (kbd "C-c C-i") 'anything-sage-shell)
 ;;   (local-set-key (kbd "C-c C-d") 'anything-sage-shell-describe-object-at-point)
